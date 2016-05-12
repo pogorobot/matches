d9f8ecade3e3fecfc68eb23a0b4fdd47ed6aa5ac
Jade Eklund <jade.eklund@gmail.com>
2016-05-12 15:10:53 -0700

    Navigation is stateful

--- build/nav.js
+++ build/nav.js
@@ -1,46 +1,50 @@
 var NavBar = React.createClass({
   displayName: "NavBar",
 
+  getInitialState: function () {
+    return {
+      visible: true,
+      active: "Eggs"
+    };
+  },
   render: function () {
     return React.createElement(
       "ul",
       { className: "nav" },
+      React.createElement(NavButton, { name: "Eggs", active: this.isActive("Eggs"), action: this.activate }),
+      React.createElement(NavButton, { name: "Webs", active: this.isActive("Webs"), action: this.activate }),
+      React.createElement(NavButton, { name: "Friends", active: this.isActive("Friends"), action: this.activate }),
+      React.createElement(NavButton, { name: "Family", active: this.isActive("Family"), action: this.activate })
+    );
+  },
+  activate: function (name) {
+    this.setState({
+      active: name
+    });
+  },
+  isActive: function (name) {
+    return this.state.active == name;
+  }
+});
+
+var NavButton = React.createClass({
+  displayName: "NavButton",
+
+  render: function () {
+    return React.createElement(
+      "li",
+      null,
       React.createElement(
-        "li",
-        null,
-        React.createElement(
-          "a",
-          { href: "http://whisperedscreams.bandcamp.com/" },
-          "Eggs"
-        )
-      ),
-      React.createElement(
-        "li",
-        null,
-        React.createElement(
-          "a",
-          null,
-          "Webs"
-        )
-      ),
-      React.createElement(
-        "li",
-        null,
-        React.createElement(
-          "a",
-          null,
-          "Friends"
-        )
-      ),
-      React.createElement(
-        "li",
-        null,
-        React.createElement(
-          "a",
-          null,
-          "Family"
-        )
+        "a",
+        { className: this.active(), href: "#", onClick: this.activate },
+        this.props.name
       )
     );
+  },
+  active: function () {
+    return this.props.active ? 'active' : 'inactive';
+  },
+  activate: function () {
+    this.props.action(this.props.name);
   }
 });
--- nav.js
+++ nav.js
@@ -1,12 +1,44 @@
 var NavBar = React.createClass({
+  getInitialState: function() {
+    return {
+      visible: true,
+      active: "Eggs"
+    };
+  },
   render: function() {
     return (
       <ul className="nav">
-        <li><a href="http://whisperedscreams.bandcamp.com/">Eggs</a></li>
-        <li><a>Webs</a></li>
-        <li><a>Friends</a></li>
-        <li><a>Family</a></li>
+        <NavButton name="Eggs" active={this.isActive("Eggs")} action={this.activate} />
+        <NavButton name="Webs" active={this.isActive("Webs")} action={this.activate} />
+        <NavButton name="Friends" active={this.isActive("Friends")} action={this.activate} />
+        <NavButton name="Family" active={this.isActive("Family")} action={this.activate} />
       </ul>
     );
   },
+  activate: function(name) {
+    this.setState({
+      active: name
+    });
+  },
+  isActive: function(name) {
+    return this.state.active == name;
+  }
+});
+
+var NavButton = React.createClass({
+  render: function() {
+    return(
+      <li>
+        <a className={this.active()} href="#" onClick={this.activate}>
+          {this.props.name}
+        </a>
+      </li>
+    );
+  },
+  active: function() {
+    return this.props.active ? 'active' : 'inactive';
+  },
+  activate: function() {
+    this.props.action(this.props.name);
+  }
 });
