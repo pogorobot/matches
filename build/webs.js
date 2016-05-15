1476171ce1ddfc344826f5ab7556ade3d69c0bb3
Jade Eklund <jade.eklund@gmail.com>
2016-05-15 16:34:32 -0700

    Some work on the tactical side of things

--- build/webs.js
+++ build/webs.js
@@ -5,26 +5,36 @@ var Webs = React.createClass({
     return React.createElement(
       "div",
       { className: "webs" },
-      React.createElement(Tile, { image: "assets/web.png" }),
-      React.createElement(Tile, { image: "assets/web.png" }),
-      React.createElement(Tile, { image: "assets/web.png" }),
-      React.createElement(Tile, { image: "assets/hatching.png" }),
-      React.createElement(Tile, { image: "assets/web.png" }),
-      React.createElement(Tile, { image: "assets/skull.png" }),
-      React.createElement(Tile, { image: "assets/web.png" }),
-      React.createElement(Tile, { image: "assets/confused.jpg" }),
-      React.createElement(Tile, { image: "assets/chess.png" }),
-      React.createElement(Tile, { image: "assets/chess.png" }),
-      React.createElement(Tile, { image: "assets/chess.png" }),
-      React.createElement(Tile, { image: "assets/chess.png" })
+      this.board().map(function (tile) {
+        return React.createElement(Tile, { image: tile.image, key: tile.key });
+      })
     );
+  },
+  board: function () {
+    var list = [];
+    for (var x = 0; x < 8; x++) {
+      for (var y = 0; y < 8; y++) {
+        list.push({ key: "x" + x + "y" + y });
+      }
+    }
+    return list;
   }
 });
 
 var Tile = React.createClass({
   displayName: "Tile",
 
+  getInitialState: function () {
+    return {
+      image: "assets/chess.png"
+    };
+  },
   render: function () {
-    return React.createElement("img", { className: "tile", src: this.props.image });
+    return React.createElement("img", { className: "tile", onClick: this.layEgg, src: this.state.image });
+  },
+  layEgg: function () {
+    this.setState({
+      image: "assets/egg.png"
+    });
   }
 });
--- webs.js
+++ webs.js
@@ -2,27 +2,37 @@ var Webs = React.createClass({
   render: function() {
     return (
       <div className="webs">
-        <Tile image="assets/web.png" />
-        <Tile image="assets/web.png" />
-        <Tile image="assets/web.png" />
-        <Tile image="assets/hatching.png" />
-        <Tile image="assets/web.png" />
-        <Tile image="assets/skull.png" />
-        <Tile image="assets/web.png" />
-        <Tile image="assets/confused.jpg" />
-        <Tile image="assets/chess.png" />
-        <Tile image="assets/chess.png" />
-        <Tile image="assets/chess.png" />
-        <Tile image="assets/chess.png" />
+        {this.board().map(function(tile) {
+          return (<Tile image={tile.image} key={tile.key} />);
+        })}
       </div>
     );
   },
+  board: function() {
+    var list = [];
+    for (var x = 0; x < 8; x++) {
+      for (var y = 0; y < 8; y++) {
+        list.push({key: "x" + x + "y" + y});
+      }
+    }
+    return list;
+  }
 });
 
 var Tile = React.createClass({
+  getInitialState: function() {
+    return {
+      image: "assets/chess.png"
+    };
+  },
   render: function() {
     return (
-      <img className="tile" src={this.props.image} />
+      <img className="tile" onClick={this.layEgg} src={this.state.image} />
     );
+  },
+  layEgg: function() {
+    this.setState({
+      image: "assets/egg.png"
+    });
   }
 });
